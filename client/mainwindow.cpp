209caefc09a3768405e8f0809b20066f3a8dd161
Rito-492 <richardtowne8284@163.com>
2024-11-14 19:06:21 +0800

    init

---
+++ client/mainwindow.cpp
@@ -0,0 +1,62 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "titleBar.h"
+#include <QVBoxLayout>
+#include <QMouseEvent>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    setWindowFlags(Qt::FramelessWindowHint);
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowMinimizeButtonHint);
+
+    m_titleBar = new TitleBar(this);
+    connect(m_titleBar, &TitleBar::minimizeClicked, this, &MainWindow::showMinimized);
+    connect(m_titleBar, &TitleBar::maximizeClicked, this, [this]() {
+        if (isMaximized()) {
+            showNormal();
+        } else {
+            showMaximized();
+        }
+    });
+    connect(m_titleBar, &TitleBar::closeClicked, this, &MainWindow::close);
+    connect(m_titleBar, &TitleBar::dragStarted, this, &MainWindow::startDragging);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(m_titleBar);
+    mainLayout->addStretch();
+
+    QWidget *centralWidget = new QWidget(this);
+    centralWidget->setLayout(mainLayout);
+    setCentralWidget(centralWidget);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::startDragging(const QPoint &globalPos)
+{
+    m_dragging = true;
+    m_dragPosition = globalPos - frameGeometry().topLeft();
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (m_dragging && (event->buttons() & Qt::LeftButton)) {
+        move(event->globalPosition().toPoint() - m_dragPosition);
+        event->accept();
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        m_dragging = false;
+        event->accept();
+    }
+}
---
+++ client/titlebar.h
@@ -0,0 +1,31 @@
+#ifndef TITLEBAR_H
+#define TITLEBAR_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QMouseEvent>
+
+class TitleBar : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit TitleBar(QWidget *parent = nullptr);
+
+signals:
+    void minimizeClicked();
+    void maximizeClicked();
+    void closeClicked();
+    void dragStarted(const QPoint &globalPos);
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+
+private:
+    QPushButton *themeButton;
+    QPushButton *minimizeButton;
+    QPushButton *maximizeButton;
+    QPushButton *closeButton;
+};
+
+#endif // TITLEBAR_H
